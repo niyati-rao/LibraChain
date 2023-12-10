9a8a0c3164c663cf5547339ac6a31718a3f6bd34
Niyati <niyati.peanut@gmail.com>
2023-12-11 01:29:42 +0530

    server added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; // Use the specified port or default to 3000
+
+// Define the path to your static HTML file
+const staticFilePath = path.join(__dirname, 'public', 'index.html');
+
+// Set up middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(staticFilePath);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
